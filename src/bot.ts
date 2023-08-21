777f839b246a2724ee0865ee6fb02072d0885f9d
Magomed <scrythepvp@gmail.com>
2023-08-21 14:56:53 +0200

    feat: add walk forward and add reach and cooldown to rest too
    added walk forward and made it so the reach and the cooldown
    works for every yet attack

--- src/bot.ts
+++ src/bot.ts
@@ -7,8 +7,11 @@ class PvPBot {
   private hitWhenInRange: boolean;
   private critDeflect: boolean;
   private punishCrits: boolean;
+  private walkForward: boolean;
   private currentHealth: number;
   private inRange: boolean;
+  private ticksTilAttack: number = 0;
+  private swordAttackCoolDown: number;
 
   constructor(bot: Bot) {
     this.thisBinder();
@@ -22,20 +25,38 @@ class PvPBot {
   }
 
   private update() {
+    this.ticksTilAttack--;
     this.lookAtTargetHead();
     this.updateIfInRange();
-    if (this.hitWhenInRange && this.inRange) this.sprintHit();
-    if (this.punishCrits && this.checkIfFalling()) {
+    if (this.hitWhenInRange && this.inRange && this.hasAtttackCooldown()) {
+      this.sprintHit();
+      this.ticksTilAttack = this.swordAttackCoolDown;
+    }
+
+    if (
+      this.punishCrits &&
+      this.inRange &&
+      this.checkIfFalling() &&
+      this.hasAtttackCooldown()
+    ) {
       this.bot.setControlState('sprint', false);
       this.bot.attack(this.target);
       this.bot.setControlState('sprint', true);
+      this.ticksTilAttack = this.swordAttackCoolDown;
     }
   }
 
   private start(username: string, message: string) {
+    this.swordAttackCoolDown = this.getSwordAttackCoolDown();
     this.target = this.bot.players[username].entity;
-    [this.hitWhenInRange, this.critDeflect, this.punishCrits] =
-      this.convertMessageToBoolArgs(message);
+    [
+      this.hitWhenInRange,
+      this.critDeflect,
+      this.punishCrits,
+      this.walkForward,
+    ] = this.convertMessageToBoolArgs(message);
+    this.bot.setControlState('forward', true);
+    if (this.walkForward) this.bot.setControlState('forward', true);
     this.bot.on('physicTick', this.update);
     this.bot.on('health', this.onDamage);
   }
@@ -51,7 +72,10 @@ class PvPBot {
       return;
     }
     this.currentHealth = this.bot.health;
-    if (this.critDeflect) this.sprintHit();
+    if (this.critDeflect && this.inRange && this.hasAtttackCooldown()) {
+      this.sprintHit();
+      this.ticksTilAttack = this.swordAttackCoolDown;
+    }
   }
 
   private sprintHit() {
@@ -83,6 +107,15 @@ class PvPBot {
 
   private checkIfFalling() {
     return this.bot.entity.velocity.y < -0.13;
+  }
+  private getSwordAttackCoolDown() {
+    const cooldown = (1 / 1.6) * 20;
+    // const minCooldown = cooldown * 0.85;
+    return cooldown;
+  }
+
+  private hasAtttackCooldown() {
+    return this.ticksTilAttack <= 0;
   }
 }
 
